bb348f378b0789fb17048d9681102645c37ddda0
Aftermaths <fnzzhou@gmail.com>
2015-08-24 17:55:55 +0100

    results page

--- spec/javascript/sudokuFeaturesSpec.js
+++ spec/javascript/sudokuFeaturesSpec.js
@@ -10,4 +10,8 @@ describe('feature testing', function() {
     expect('h1').toContainText('Have fun');
   });
 
+  it('redirects to results page', function() {
+    $('input#finish_game').click();
+    expect('h1').toContainText('Result');
+  });
 });
