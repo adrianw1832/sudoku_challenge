7741ce15bdc396e0a3eac77894ba81889e2ba522
Aftermaths <fnzzhou@gmail.com>
2015-08-25 17:46:58 +0100

    trying to fix results

--- app/public/javascript/application.js
+++ app/public/javascript/application.js
@@ -1,31 +1,24 @@
 $(document).ready(function() {
   var sudoku = new Sudoku();
+  var startTime = new Date();
 
   $('input#start_game_button').click(function() {
     window.location.assign('http://127.0.0.1:9292/game');
-    Sudoku.prototype.startTime = new Date();
   });
 
   $('input#finish_game').click(function() {
-     if (sudoku.isGameFinished()) {
-      Sudoku.prototype.endTime = new Date();
-      var time = resultTime();
+    if (sudoku.isGameFinished()) {
+      var endTime = new Date();
+      var time = (endTime - startTime) / 1000;
       sendResult(time);
       window.location.assign('http://127.0.0.1:9292/results');
     }
   });
 
   var sendResult = function(time) {
-    $.post('http://127.0.0.1:9292/results', { seconds: 60 });
-    // $.ajax({
-    //   method: 'POST',
-    //   url: 'http://127.0.0.1:9292/results',
-    //   data: { seconds: time }
-    // });
-  };
-
-  var resultTime = function(time) {
-    return (sudoku.startTime - sudoku.endTime)/1000;
+    $.post('http://127.0.0.1:9292/results', {
+      seconds: time
+    });
   };
 
   var buildGUI = function() {
