7c1e7dc49423e6f87b812a1bd4c493ca801a1a52
Leon Wee <leonwee27@gmail.com>
2015-08-25 18:13:29 +0700

    Can't start game without being signed in

--- spec/features/playing_a_game_spec.rb
+++ spec/features/playing_a_game_spec.rb
@@ -1,5 +1,19 @@
 feature 'on the homepage' do
 
+  context '#Starting a game' do
+    scenario 'player cannot start game unless signed in' do
+      visit('/')
+      expect(page).not_to have_button('Start Game')
+    end
+
+    scenario 'player can start game when signed in' do
+      user = create(:user)
+      sign_in_as(user)
+      visit('/')
+      expect(page).to have_button('Start Game')
+    end
+  end
+
   scenario 'starting a game' do
     visit '/game'
     expect(page).to have_content 'Have fun'
